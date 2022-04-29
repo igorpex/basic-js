867afb6c440fae2023218fcb725355d746a61283
igorpex <igor.bogdanov@gmail.com>
2022-04-30 01:58:32 +0300

    feat: transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,60 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error("'arr' parameter must be an instance of the Array!");
+    // return "'arr' parameter must be an instance of the Array!"
+  }
+  if (arr.length === 0) {
+    return [];
+  }
+
+  let result = [];
+  let doubleNext = false;
+  let discardNext = false;
+  let newArr = [...arr]
+  newArr.forEach((element, index, array) => {
+    if (doubleNext) {
+      if (element) {
+        result.push(element);
+        result.push(element);
+      }
+      doubleNext = false;
+    } else if (discardNext) {
+      let previousResultElem = result[result.length - 1];
+      if (previousResultElem === element) {
+        result.pop();
+      }
+      discardNext = false;
+    } else if (element === "--double-next") {
+      doubleNext = true;
+    } else if (element === "--discard-next") {
+      discardNext = true;
+      array[index + 1] = null;
+    } else if (element === "--discard-prev") {
+      let previousElem = array[index - 1];
+      let previousResultElem = result[result.length - 1];
+      if (previousResultElem === previousElem) {
+        result.pop();
+      }
+      // result = result.filter(resElement => resElement !== previousElem)
+    } else if (element === "--double-prev") {
+      let doubleElement = array[index - 1];
+      if (doubleElement) {
+        result.push(doubleElement);
+      }
+    } else if (element === undefined) {
+      console.log("hello");
+    } else {
+      if (element) {
+        result.push(element);
+      }
+
+    }
+  }
+  )
+  return result
 }
 
 module.exports = {
